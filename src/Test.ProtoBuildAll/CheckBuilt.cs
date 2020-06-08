3612f298d492c6bd3856755df5d52682716e4ee2
Michael Barber <60610888+mike-barber@users.noreply.github.com>
2020-06-08 12:10:17 +0100

    added enum check

--- src/Test.ProtoBuildAll/CheckBuilt.cs
+++ src/Test.ProtoBuildAll/CheckBuilt.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using Google.Protobuf;
 using Xunit;
 
@@ -22,13 +22,14 @@ namespace Test.Common
         [Fact]
         public void UseMessages()
         {
-            var payload = new Package2.Namespace2.Payload() {
+            var payload = new Package2.Namespace2.Payload()
+            {
                 Name = "payload",
                 List = new Package1.Namespace2.ListOfThings()
             };
 
             payload.List.Things1.Add(new Package1.Namespace1.Thing1 { Name = "thing1" });
-            payload.List.Things2.Add(new Package1.Namespace1.Thing2 { Name = "thing2 "});
+            payload.List.Things2.Add(new Package1.Namespace1.Thing2 { Name = "thing2 " });
 
             // serialize
             var bytes = payload.ToByteArray();
@@ -37,6 +38,23 @@ namespace Test.Common
             // deserialize
             var deserialized = Package2.Namespace2.Payload.Parser.ParseFrom(bytes);
             deserialized.MergeFrom(bytes);
+        }
+
+        [Fact]
+        public void UseEnum()
+        {
+            var msg1 = new Package1.Namespace1.Thing1()
+            {
+                Name = "thing1",
+                Kind = Package1.Namespace1.TestEnum.First
+            };
+            Assert.NotNull(Package1.Namespace1.Thing1.Parser.ParseFrom(msg1.ToByteArray()));
+
+            var payload = new Package2.Namespace2.Payload()
+            {
+                Kind = Package1.Namespace1.TestEnum.Second
+            };
+            Assert.NotNull(Package2.Namespace2.Payload.Parser.ParseFrom(payload.ToByteArray()));
         }
     }
 }
